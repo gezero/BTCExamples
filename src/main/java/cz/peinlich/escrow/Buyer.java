db2e67a5b7e33d6f2bfa785999293b48e0ce1e3b
george <jiri.peinlich@gmail.com>
2013-11-30 09:49:38 +0100

    buyer is chalanging escrow

--- src/main/java/cz/peinlich/escrow/Buyer.java
+++ src/main/java/cz/peinlich/escrow/Buyer.java
@@ -1,6 +1,7 @@
 package cz.peinlich.escrow;
 
 import com.google.bitcoin.core.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +14,27 @@ import java.security.PublicKey;
  */
 @Component
 public class Buyer implements CanSignTransactions{
+    @Autowired
+    Escrow escrow;
+    private String nonce = "random nonce should this be";
+
     public Transaction createDepositTransaction(PublicKey sellerPublicKey, PublicKey escrowPublicKey) {
+        challengeEscrow(escrowPublicKey);
         throw new UnsupportedOperationException("will get here");
     }
 
     @Override
     public PublicKey generateNewPublicKey() {
         return null;  //TODO: this is default template
+    }
+
+
+    private void challengeEscrow(PublicKey escrowPublicKey) {
+        String signature = escrow.pleaseSignNonce(escrowPublicKey, nonce);
+        checkSignature(signature, escrowPublicKey);
+    }
+
+    private void checkSignature(String nonce, PublicKey escrowPublicKey) {
+        throw new UnsupportedOperationException("will get here sooner or later");
     }
 }
