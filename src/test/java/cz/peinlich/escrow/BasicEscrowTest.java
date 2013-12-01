d57ae3f1cdcd6d8eab18635c15c3804a2b96c408
george <jiri.peinlich@gmail.com>
2013-12-01 09:23:19 +0100

    wallets are now syncing

--- src/test/java/cz/peinlich/escrow/BasicEscrowTest.java
+++ src/test/java/cz/peinlich/escrow/BasicEscrowTest.java
@@ -1,6 +1,9 @@
 package cz.peinlich.escrow;
 
 import cz.peinlich.configuration.EscrowConfiguration;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Period;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
 
 /**
  * User: George
@@ -39,14 +44,30 @@ public class BasicEscrowTest {
      * After the sync finishes, the test stops and you can play with the other tests, if you will not run this one first
      * the sync will be done in some other test and you will just not know when it finished.
      *
+     * There will be always some sync on the beginning of test that is syncing new blocks. You can check that everything is
+     * synced by running this test for second time. It will end much much much faster.
+     *
      *
      */
     @Test
     public void synchronizeWithNetwork(){
+        DateTime start = new DateTime();
+        logger.info("Syncing buyer. Started at {}", start);
         buyer.start();
-        seller.start();
-        escrow.start();
+        DateTime buyerComplete = new DateTime();
+        logger.info("Buyer synced. It took {}.", new Duration(start,buyerComplete));
 
+        logger.info("Syncing Seller.");
+        seller.start();
+        DateTime sellerComplete = new DateTime();
+        logger.info("Seller synced. It took {}.", new Duration(buyerComplete,sellerComplete));
+
+        logger.info("Syncing Escrow");
+        escrow.start();
+        DateTime escrowComplete = new DateTime();
+        logger.info("Escrow synced. It took {}.", new Duration(sellerComplete,escrowComplete));
+
+        logger.info("Sync complete. Alltogether it took {}.", new Duration(start,escrowComplete));
     }
 
 
